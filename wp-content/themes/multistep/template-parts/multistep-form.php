34a6fce1dca8ea4702357bc2b304cb16444a73ee
antonmiazyn <miazyn.anton@gmail.com>
2022-01-19 11:39:03 +0200

    Write form settings to .json

--- wp-content/themes/multistep/template-parts/multistep-form.php
+++ wp-content/themes/multistep/template-parts/multistep-form.php
@@ -1,1 +1,10 @@
+<?php
+  # Multistep form inherit settings
+  $form = get_field("multistep_form");
+
+  # Fill in data
+  $json = json_encode(array('data' => $form));
+  file_put_contents("wp-content/themes/" . get_current_theme() . "/js/multistep-form/multistep-settings.json", $json);
+?>
+
 <div id="multistep"></div>
